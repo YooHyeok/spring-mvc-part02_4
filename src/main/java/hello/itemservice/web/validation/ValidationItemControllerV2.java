94c8e2bfaa829a44ed029ac008cd4f04450e5052
YooHyeok <yjk7454@naver.com>
2023-04-13 16:32:21 +0900

    errors.properties 추가 및 application.properties에 등록 설정

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -101,6 +101,7 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
+
 //    @PostMapping("/add")
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult,
                           /*
