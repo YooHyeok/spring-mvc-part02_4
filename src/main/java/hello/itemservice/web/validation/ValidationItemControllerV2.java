ef7241204f7b76ec0a8d2583e172793c57080e25
YooHyeok <yjk7454@naver.com>
2023-04-13 17:42:53 +0900

    addItemV4()메소드 추가 및 rejectValue() 처리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,6 +45,64 @@ public class ValidationItemControllerV2 {
     }
 
     @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes, Model model) {
+        log.info("objectName={}", bindingResult.getObjectName());
+        log.info("target={}", bindingResult.getTarget());
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+//            bindingResult.addError(new FieldError("item","itemName", "상품 이름은 필수입니다."));
+//            bindingResult.addError(new FieldError("item","itemName", item.getItemName(), false, null, null, "상품 이름은 필수입니다."));
+            /**
+             * 타임리프에서는 정상적인 상황에서는 Model객체의 값을 읽지만, BindingResult를 통해 Error발생시 Model로부터 값을 읽지 않고
+             * FieldError에서 rejectedValue에 보관한 값을 사용해서 값을 추출한다.
+             * rejectedValue - 거절된값 : item.getItemName()
+             * bindingFailure - 데이터 바인딩실패 : false (데이터자체는 제대로 들어왔으므로)
+             * code : null
+             * arguments : null
+             */
+//            bindingResult.addError(new FieldError("item","itemName", item.getItemName(), false,new String[]{"required.item.itemName"},null, null));
+            bindingResult.rejectValue("itemName", "required");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+//            bindingResult.addError(new FieldError("item", "price", "가격은 1,000원 에서 1,000,000원 까지 허용합니다."));
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, null, null, "가격은 1,000원 에서 1,000,000원 까지 허용합니다."));
+//            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false,new String[]{"range.item.price"}, new Object[]{1000, 1000000}, null));
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+//            bindingResult.addError(new FieldError("item", "quantity", "수량은 최대 9,999 까지 허용합니다."));
+//            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false,null, null, "수량은 최대 9,999 까지 허용합니다."));
+//            bindingResult.addError(new FieldError("item", "quantity", item.getQuantity(), false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+        // 특정 필드가 아닌 복합 검증
+        if (item.getPrice() != null && item.getQuantity() != null) { //금액과 수량 모두 null이 아닌경우
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) { // 10000원 미만일 경우
+//                bindingResult.addError(new ObjectError("item", null, null,"가격 * 수량의 합은 10,000원 이상이어야 합니다. 현재값 = " + resultPrice));
+//                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[] {10000, resultPrice}, null));
+                bindingResult.reject("totalPriceMin", new Object[] {10000, resultPrice}, null);
+            }
+
+        }
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) { //에러가 비어있지 않으면 (에러가 존재하면)
+            log.info("bindingResult = {}", bindingResult);
+            /**
+             * bindingResult는 자동으로 view에 넘어가는 모델 역할을 한다.
+             */
+            return "validation/v2/addForm";
+        }
+
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+//    @PostMapping("/add")
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult,
                             RedirectAttributes redirectAttributes, Model model) {
         //검증 로직
